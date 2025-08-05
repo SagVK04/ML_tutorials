baea6dec1eff90b41ba74abffbf9ecef66bd9d13
Sagnik Metiya <metiyasagnik692@gmail.com>
2025-08-05 21:49:28 +0530

    ML Tutorial Day 4 (Fraud Detector Model  Modified)

--- fraud_detector_1.py
+++ fraud_detector_1.py
@@ -2,8 +2,9 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('upi_transactions.csv')
+df = pd.read_csv('upi_transactions_7000.csv')
 
 #KNN Classifiers need all feature values in digits
 
@@ -18,25 +19,38 @@ X = df.drop(columns=['Fraud Possibility'])
 
 Y = df['Fraud Possibility']
 
-X_train = X[:-150]
-X_test = X[-140:]
+X_train = X[:-1400]
+X_test = X[-1340:]
 #Standardizing the features(X) to increase accuracy
 scaled = StandardScaler()
 X_train_scaled = scaled.fit_transform(X_train)
 X_test_scaled = scaled.fit_transform(X_test)
 
-Y_train = Y[:-150]
-Y_test = Y[-140:]
+Y_train = Y[:-1400]
+Y_test = Y[-1340:]
 
-fraud_model = KNeighborsClassifier(n_neighbors=95)
-fraud_model.fit(X_train_scaled,Y_train)
+fraud_model = KNeighborsClassifier(n_neighbors=1113)
+fraud_model.fit(X_train_scaled, Y_train)
 Y_pred = fraud_model.predict(X_test_scaled)
-acc = accuracy_score(Y_test,Y_pred)*100
 
-if(acc <=50):
-    print("Transaction may be fraud!")
-else:
-    print("Transaction is safe")
+print(f"Accuracy: {accuracy_score(Y_test,Y_pred)*100:.2f} %")
+
+accuracy_scores = []
+neighbors_range = range(1, 600)
+
+for k in neighbors_range:
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(X_train_scaled, Y_train)
+    Y_pred = model.predict(X_test_scaled)
+    accuracy = accuracy_score(Y_test, Y_pred)
+    accuracy_scores.append(accuracy)
+
+max_accuracy = max(accuracy_scores) * 100
+print(f"The maximum accuracy is: {max_accuracy:.2f} when neighbors: {k}%")
+
+
+
+
 
 
 #best_acc = 0
